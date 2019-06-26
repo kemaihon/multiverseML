c3ce1b4bd6e3558f2b44b8b3892c3dda2a7f5a83
Cristian Carlos dos Santos <perestra.ccds@gmail.com>
2019-06-25 21:35:58 -0300

    Pacote publicado em testes

--- docs.md
+++ docs.md
@@ -1,3 +1,5 @@
+publicar
+https://blog.alura.com.br/como-publicar-seu-codigo-python-no-pypi/
 git
 https://support.codebasehq.com/articles/getting-started/git-on-windows
 https://anaconda.org/anaconda/git
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='multiverseML',
-    version='0.1.1',
+    version='0.1.4-Alpha',
     url='https://github.com/cccadet/multiverseML/',
     license='GNU General Public License v3.0',
     author='Cristian Carlos dos Santos',
