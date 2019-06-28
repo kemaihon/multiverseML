5aa428ff9c649851c4053853c713716d4b032283
cccadet <perestra.ccds@gmail.com>
2019-06-28 15:30:57 -0300

    Ajuste dependências e descrição Pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='multiverseML',
     version='0.1.5-Alpha',
@@ -10,6 +15,20 @@ setup(
     author_email='perestra.ccds@gmail.com',
     keywords='multiverseML, machine-learning, pipeline',
     description=u'Aplicação para facilitar a criação de pipelines para desenvovimento de algoritmos de Machine Learning',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     packages=['multiverseml'],
-    install_requires=[],
+    install_requires=[
+        subprocess,
+        os,
+        json,
+        datetime,
+        inspect,
+        pickle,
+        shutil,
+        numpy,
+        pandas,
+        flask,
+        pandas
+    ],
 )
