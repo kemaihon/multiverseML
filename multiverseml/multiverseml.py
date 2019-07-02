902c469dbc177de7f3ff5b6e752eee658e202883
cccadet <perestra.ccds@gmail.com>
2019-07-02 17:58:39 -0300

    multivision

--- multiverseml/multiverseml.py
+++ multiverseml/multiverseml.py
@@ -140,3 +140,12 @@ def access_reality(alternative_reality):
     copyfile(alternative_reality, new_reality)
     return new_reality
 
+#multivision
+def multivision(path):
+    d = {'name': os.path.basename(path)}
+    if os.path.isdir(path):
+        d['type'] = "directory"
+        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir(path)]
+    else:
+        d['type'] = "file"
+    return json.dumps(d)
